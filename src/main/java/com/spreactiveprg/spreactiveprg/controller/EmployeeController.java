f07ac951a1ae9aaf960c418f1e0619b3eacbdbd1
Akshay Patil <akshaypatil240988@gmail.com>
2021-04-27 14:34:08 +0530

    Initial commit

---
+++ src/main/java/com/spreactiveprg/spreactiveprg/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.spreactiveprg.spreactiveprg.controller;
+
+import com.spreactiveprg.spreactiveprg.model.Employee;
+import com.spreactiveprg.spreactiveprg.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @PostMapping("/saveEmployee")
+    public Mono<Employee> saveEmployee(@RequestBody Employee employee){
+        Mono<Employee> e=employeeRepo.save(employee);
+        return e;
+    }
+
+    @GetMapping("/getemp")
+    public Flux<Employee> getList(){
+        return employeeRepo.findAll();
+    }
+}
